10d5d00df9318f056fced2a102c12c97952e4fac
Burke Strang <burke.strang@outlook.com>
2025-03-07 22:08:26 -0500

    wip

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,5 +5,14 @@ namespace opentelem.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
-    public DbSet<Product> Products { get; set; }
+  public DbSet<Product> Products { get; set; }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    base.OnModelCreating(modelBuilder);
+
+    modelBuilder.Entity<Product>()
+        .Property(p => p.Price)
+        .HasPrecision(18, 2);
+  }
 }
